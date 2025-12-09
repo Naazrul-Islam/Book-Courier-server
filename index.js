880b228fc67f76e5020d82764c01ae7546d46331
Naazrul-Islam <hmnazrulislam06@gmail.com>
2025-12-09 06:27:51 -0600

    some problem solved added

--- index.js
+++ index.js
@@ -1,42 +1,49 @@
-const express = require('express')
-const cors = require('cors')
-const app = express()
-require('dotenv').config()
+const express = require('express');
+const cors = require('cors');
+const app = express();
+require('dotenv').config();
 
-const { MongoClient, ServerApiVersion } = require('mongodb')
-const port = process.env.PORT || 3000
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb'); 
+const port = process.env.PORT || 3000;
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.c0vwcej.mongodb.net/?appName=Cluster0`
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.c0vwcej.mongodb.net/?appName=Cluster0`;
 
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
-})
+  },
+});
 
 async function run() {
   try {
-    await client.connect()
+    await client.connect();
 
-    const db = client.db("bookCourier")
-    const booksCollection = db.collection("books")
+    const db = client.db("bookCourier");
+    const booksCollection = db.collection("books");
 
-    await client.db("admin").command({ ping: 1 })
-    console.log("Connected to MongoDB!")
+    console.log("Connected to MongoDB!");
 
-    // Get all books
+    // GET all books
     app.get("/books", async (req, res) => {
-      const result = await booksCollection.find().toArray()
-      res.send(result)
-    })
+      const status = req.query.status;
+      const query = status ? { status } : {};
+      const result = await booksCollection.find(query).toArray();
+      res.send(result);
+    });
 
-    // 
-    //  Add New Book (POST API)
+    // GET single book
+    app.get("/books/:id", async (req, res) => {
+      const id = req.params.id;
+      const result = await booksCollection.findOne({ _id: new ObjectId(id) });
+      res.send(result);
+    });
+
+    // POST add new book
     app.post("/books", async (req, res) => {
       try {
         const newBook = req.body;
@@ -47,17 +54,29 @@ async function run() {
       }
     });
 
+    // PUT update book
+    app.put("/books/:id", async (req, res) => {
+      const id = req.params.id;
+      const updatedBook = req.body;
+
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = { $set: updatedBook };
+
+      const result = await booksCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 }
 
-run().catch(console.dir)
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
-  res.send('Bookcourier Server!')
-})
+  res.send('Bookcourier Server!');
+});
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`)
-})
+  console.log(`Server running on port ${port}`);
+});
