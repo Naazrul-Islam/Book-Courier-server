130c8affce8ab4c37333a8d94a946fcdd684f40c
Naazrul-Islam <hmnazrulislam06@gmail.com>
2025-12-15 04:14:16 -0600

    port change added

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const stripe = require("stripe")(process.env.Stripe_Key);
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4040;
 
 app.use(cors());
 app.use(express.json());
